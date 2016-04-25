b4c8a6ba9e18ef5dcbfc6e56291df15a6517db61
frank boakye yiadom <fbyiadom@gmail.com>
2016-04-25 14:27:43 -0400

    Eidt and Delete actions and view files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -26,6 +26,24 @@ class PlaysController < ApplicationController
 		end
 	end
 
+	def edit
+		
+	end
+
+	def update
+		if @play.update(play_params)
+			redirect_to play_path(@play)
+			
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@play.destroy
+		redirect_to root_path
+	end
+
 	private
 
 	def play_params
